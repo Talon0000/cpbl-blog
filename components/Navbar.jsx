c328b6df280749db9f9d96fe5008827b17b2b458
沈晟揚 <zxc29150165@gmail.com>
2025-10-22 15:23:07 +0800

    Restore the origin code from SignOut

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -140,12 +140,11 @@ const Navbar = () => {
 									</Link> */}
 									<button
 										className="text-black tracking-wider hover:bg-gray-200 hover:text-green-700 rounded-b-md pr-24 pb-2 pt-1"
-										onClick={async () => {
+										onClick={() => {
 											setIsProfileMenuOpen(false);
-											await signOut({
-												redirect: false,
+											signOut({
+												callbackUrl: `${process.env.NEXT_PUBLIC_DOMAIN || "/"}`,
 											});
-											window.location.href = "/";
 										}}>
 										登出
 									</button>
