65417a3ae5d7d3533ec5234dfca8bc2bd263bd5f
沈晟揚 <zxc29150165@gmail.com>
2025-11-21 21:19:38 +0800

    comment out middleware.js file

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
-export const config = {
-	matcher: ["/posts/add", "/profile"],
-};
+// export const config = {
+// 	matcher: ["/posts/add", "/profile", "/posts/:path*/edit"],
+// };
